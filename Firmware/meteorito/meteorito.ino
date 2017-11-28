9e9a5735b0bfbb1ff08abe3293778b89d4a928d7
sabas1080 <s@theinventorhouse.org>
2017-11-28 10:15:26 -0600

    Agregado sensor de presion BMP180
    Presion, Altura y temperatura

--- Firmware/meteorito/meteorito.ino
+++ Firmware/meteorito/meteorito.ino
@@ -32,8 +32,13 @@ Bajo Licencia MIT
 #include  <DHT.h>
 #include <Wire.h>
 #include "Adafruit_VEML6070.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP085_U.h>
 
 Adafruit_VEML6070 uv = Adafruit_VEML6070();
+Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+
+sensors_event_t event;
 
 int sensor = D4;
 float temperatura;
@@ -67,6 +72,52 @@ char nubosidad() {
   return nubosidad;
 }
 
+void presion(){
+  /* Display the results (barometric pressure is measure in hPa) */
+  if (event.pressure)
+  {
+    /* Display atmospheric pressue in hPa */
+    Serial.print("Presion:    ");
+    Serial.print(event.pressure);
+    Serial.println(" hPa");
+    
+    /* Calculating altitude with reasonable accuracy requires pressure    *
+     * sea level pressure for your position at the moment the data is     *
+     * converted, as well as the ambient temperature in degress           *
+     * celcius.  If you don't have these values, a 'generic' value of     *
+     * 1013.25 hPa can be used (defined as SENSORS_PRESSURE_SEALEVELHPA   *
+     * in sensors.h), but this isn't ideal and will give variable         *
+     * results from one day to the next.                                  *
+     *                                                                    *
+     * You can usually find the current SLP value by looking at weather   *
+     * websites or from environmental information centers near any major  *
+     * airport.                                                           *
+     *                                                                    *
+     * For example, for Paris, France you can check the current mean      *
+     * pressure and sea level at: http://bit.ly/16Au8ol                   */
+     
+    /* Primero obtenemos la temperatura actual del BMP085 */
+    float temperature;
+    bmp.getTemperature(&temperature);
+    Serial.print("Temperatura BMP180: ");
+    Serial.print(temperature);
+    Serial.println(" C");
+
+    /* Luego convierte la presión atmosférica, y SLP a la altitud        */
+    /* Actualice esta próxima línea con el SLP actual para obtener mejores resultados     */
+    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+    Serial.print("Altitud:    "); 
+    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
+                                        event.pressure)); 
+    Serial.println(" m");
+    Serial.println("");
+  }
+  else
+  {
+    Serial.println("Sensor error");
+  }
+}
+
 /*
  *Función de envio de datos 
  */
@@ -84,7 +135,7 @@ static void envioDatos () {
   
   clouds = String(nubosidad());
   humidity = String(humedad);
-  pressure = String(random(0,150));
+  pressure = String(event.pressure);
   rain = String(random(0,250));
   temp = String(temperatura);
   ultrav = String(uv.readUV());
@@ -127,6 +178,14 @@ void setup () {
   Serial.begin(9600);
   uv.begin(VEML6070_1_T);  // pass in the integration time constant
   dht.begin();
+
+  /* Initializar el sensor BMP180 */
+  if(!bmp.begin())
+  {
+    /* Hubo un problema al detectar el BMP085 ... verifique sus conexiones */
+    Serial.print("Ooops, no detectado BMP085 ... Checar tu cableado o I2C Direccion!");
+    while(1);
+  }
   
   Serial.println("\n Iniciando ejemplo de Envio");
 
@@ -146,7 +205,12 @@ void setup () {
 void loop () {
   temperatura = dht.readTemperature();
   humedad = dht.readHumidity();
- 
+   
+  /* Obtener una nueva lectura del sensor BMP180 */ 
+  bmp.getEvent(&event);
+  
+  presion();
+  
   envioDatos();
 
 //Recibe respuesta del servidor
